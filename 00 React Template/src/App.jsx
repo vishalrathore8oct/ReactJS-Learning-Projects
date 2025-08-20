e77a18af9b0f92ac0cd91a4afd1a0b87d8484fc0
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-08-20 10:52:00 +0530

    create a react project starter template using vite and tailwind css

---
+++ 00 React Template/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline text-center text-blue-500">Starter Templet of React App Using Vite !</h1>
+    </>
+  )
+}
+
+export default App
